1c5ba9dd8c03a8c7da3a9b67592dea2848326b68
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2024-12-12 18:29:14 +0700

    remove hand_sign backend code

--- api/base.py
+++ api/base.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from .routes.hand_sign import router as hand_sign_router
 from .routes.user import router as user_router
-from .routes.voice_sign import router as voice_sign_router
+
 
 api_router = APIRouter()
 api_router.include_router(hand_sign_router, tags=["hand_sign"])
 api_router.include_router(user_router, tags=["user"])
-api_router.include_router(voice_sign_router, tags=["voice_sign"])
+
 
